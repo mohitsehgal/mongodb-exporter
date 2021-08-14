b876de3377764bd6876ae162c0cc9aed09a9b98f
Mohit Sehgal <sehgal.mohit29@gmail.com>
2021-08-14 11:58:43 +0530

    Update Dependencies

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
