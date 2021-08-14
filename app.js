a3d80af96f8cd77cbf96dfdf3fb5f7c2c333cfb8
Mohit Sehgal <sehgal.mohit29@gmail.com>
2021-08-14 11:53:33 +0530

    use let

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 const cors = require('cors')
 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,12 +20,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var whitelist = [
+let whitelist = [
   'http://localhost:3000',
   'http://localhost:8080'
 ]
 let corsOptionsDelegate = function (req, callback) {
-  var corsOptions
+  let corsOptions
   if (whitelist.indexOf(req.header('Origin')) !== -1) {
       corsOptions = {
           origin: true,
@@ -43,7 +42,6 @@ app.use(cors(corsOptionsDelegate))
 app.options('*', cors())
  
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 const MainController = require('../controllers/index');
 /* GET home page. */
